0b5ba152f2132e28070078f2b9fa2d64a0291094
Baklanozavr <abaklan88@gmail.com>
2018-10-09 12:54:47 +0700

    Сделал конструкторы без выравнивания строк матрицы по длине.

--- Matrix/src/ru/academits/baklanov/tasks/Matrix.java
+++ Matrix/src/ru/academits/baklanov/tasks/Matrix.java
@@ -1,6 +1,59 @@
 package ru.academits.baklanov.tasks;
 
-import ru.academits.baklanov.tasks.Vector;
-
 public class Matrix {
+    private Vector[] rows;
+
+    public Matrix(int n, int m) {
+        this(new double[n][m]);
+    }
+
+    public Matrix(Matrix matrix) {
+        if (matrix == null) {
+            throw new IllegalArgumentException("Матрица не найдена! (null)");
+        }
+
+        rows = new Vector[matrix.rows.length];
+
+        for (int i = 0; i < matrix.rows.length; ++i) {
+            rows[i] = new Vector(matrix.rows[i]);
+        }
+    }
+
+    public Matrix(double[][] array2D) {
+        //предполагается, что все вложенные массивы одного размера
+        if (array2D == null) {
+            throw new IllegalArgumentException("Массив не найден! (null)");
+        }
+
+        if (array2D.length == 0 || array2D[0].length == 0) {
+            throw new IllegalArgumentException("Размерность матрицы должна быть больше ноля!");
+        }
+
+        rows = new Vector[array2D.length];
+
+        for (int i = 0; i < array2D.length; ++i) {
+            rows[i] = new Vector(array2D[i]);
+        }
+    }
+
+    public Matrix(Vector[] vectors) {
+        if (vectors == null) {
+            throw new IllegalArgumentException("Массив не найден! (null)");
+        }
+
+        rows = new Vector[vectors.length];
+
+        for (int i = 0; i < vectors.length; ++i) {
+            rows[i] = new Vector(vectors[i]);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder("{ " + rows[0]);
+        for (int i = 1; i < rows.length; ++i) {
+            result.append(", ").append(rows[i]);
+        }
+        return result.append(" }").toString();
+    }
 }
--- Matrix/src/ru/academits/baklanov/testMatrix/Main.java
+++ Matrix/src/ru/academits/baklanov/testMatrix/Main.java
@@ -1,4 +1,34 @@
 package ru.academits.baklanov.testMatrix;
 
+import ru.academits.baklanov.tasks.Matrix;
+import ru.academits.baklanov.tasks.Vector;
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double[][] testArray2D = {{1, 2}, {2, 3, 4}};
+
+        double[] testArray1 = {1, 2};
+        double[] testArray2 = {5, 3, 1};
+
+        Vector[] vectors = new Vector[2];
+        vectors[0] = new Vector(testArray1);
+        vectors[1] = new Vector(testArray2);
+
+        System.out.println("Введите размерность матрицы (два целых числа):");
+        Matrix matrix1 = new Matrix(scanner.nextInt(), scanner.nextInt());
+        System.out.printf("Тест инициализации матрицы1 через размерность: %s%n", matrix1);
+
+        Matrix matrix2 = new Matrix(matrix1);
+        System.out.printf("Тест инициализации матрицы2 через копирование матрицы 1: %s%n", matrix2);
+
+        Matrix matrix3 = new Matrix(testArray2D);
+        System.out.printf("Тест инициализации матрицы3 через двумерный массив: %s%n", matrix3);
+
+        Matrix matrix4 = new Matrix(vectors);
+        System.out.printf("Тест инициализации матрицы4 через массив векторов: %s%n", matrix4);
+    }
 }

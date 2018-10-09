4f4212745491c25f7870463ea171f9b3c25c6f4c
Baklanozavr <abaklan88@gmail.com>
2018-10-09 11:42:27 +0700

    Создал модуль Matrix, использующий модуль Vector.

---
+++ Matrix/src/ru/academits/baklanov/tasks/Matrix.java
@@ -0,0 +1,6 @@
+package ru.academits.baklanov.tasks;
+
+import ru.academits.baklanov.tasks.Vector;
+
+public class Matrix {
+}
---
+++ Matrix/src/ru/academits/baklanov/testMatrix/Main.java
@@ -0,0 +1,4 @@
+package ru.academits.baklanov.testMatrix;
+
+public class Main {
+}

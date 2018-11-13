633d22a3da921830bdd919bd6d8e8187df2ea0eb
Baklanozavr <abaklan88@gmail.com>
2018-11-13 12:40:49 +0700

    Осталось написать обходы.

--- Tree/src/ru/academits/baklanov/tasks/BinaryTree.java
+++ Tree/src/ru/academits/baklanov/tasks/BinaryTree.java
@@ -1,23 +1,34 @@
 package ru.academits.baklanov.tasks;
 
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 public class BinaryTree<E extends Integer> {
     private TreeNode<E> root;
     private Comparator<E> comparator;
+    private int size;
 
     public BinaryTree() {
         root = null;
         this.comparator = new TreeComparator();
+        size = 0;
     }
 
     public E getRoot() {
-        return root.getData();
+        return root.data;
+    }
+
+    public int getSize() {
+        return size;
     }
 
     public void add(E element) {
+        if (root == null) {
+            root = new TreeNode<>(element);
+        }
+
         for (TreeNode<E> node = root; node != null; ) {
-            if (comparator.compare(element, node.getData()) < 0) {
+            if (comparator.compare(element, node.data) < 0) {
                 if (node.left != null) {
                     node = node.left;
                 } else {
@@ -31,8 +42,108 @@ public class BinaryTree<E extends Integer> {
                 }
             }
         }
+
+        ++size;
     }
 
+    public TreeNode<E> findNode(E sample) {
+        if (root == null) {
+            throw new NoSuchElementException("This tree is empty!");
+        }
+
+        for (TreeNode<E> node = root; node != null; ) {
+            if (comparator.compare(sample, node.data) < 0) {
+                node = node.left;
+            } else if (comparator.compare(sample, node.data) > 0) {
+                node = node.right;
+            } else {
+                return node;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean remove(E sample) {
+        if (root == null) {
+            throw new NoSuchElementException("This tree is empty!");
+        }
+
+        TreeNode<E> node = root;
+        TreeNode<E> parent = null;
+        boolean isLeft = false;
+
+        while (node != null) {
+            if (comparator.compare(sample, node.data) < 0) {
+                parent = node;
+                node = node.left;
+                isLeft = true;
+            } else if (comparator.compare(sample, node.data) > 0) {
+                parent = node;
+                node = node.right;
+                isLeft = false;
+            } else {
+                break;
+            }
+        }
+
+        if (node == null) {
+            return false;
+        }
+
+        if (node.left == null && node.right == null) {
+            if (parent == null) {
+                root = null;
+            } else {
+                if (isLeft) {
+                    parent.left = null;
+                } else {
+                    parent.right = null;
+                }
+            }
+        } else if (node.left == null || node.right == null) {
+            if (parent == null) {
+                root = node.left == null ? node.right : node.left;
+            } else {
+                if (isLeft) {
+                    parent.left = node.left == null ? node.right : node.left;
+                } else {
+                    parent.right = node.left == null ? node.right : node.left;
+                }
+            }
+        } else {
+            TreeNode<E> parentMinChild = node.right;
+            TreeNode<E> minChild = parentMinChild.left;
+
+            if (minChild == null) {
+                minChild = parentMinChild;
+                node.right = minChild.right;
+            } else {
+                while (minChild.left != null) {
+                    parentMinChild = minChild;
+                    minChild = minChild.left;
+                }
+
+                parentMinChild.left = minChild.right;
+            }
+
+            minChild.left = node.left;
+            minChild.right = node.right;
+
+            if (parent == null) {
+                root = minChild;
+            } else {
+                if (isLeft) {
+                    parent.left = minChild;
+                } else {
+                    parent.right = minChild;
+                }
+            }
+        }
+
+        --size;
+        return true;
+    }
 
     private class TreeComparator implements Comparator<E> {
         @Override
@@ -50,42 +161,6 @@ public class BinaryTree<E extends Integer> {
             this.data = data;
             left = null;
             right = null;
-        }
-
-        void paste(E element) {
-
-        }
-
-        T getData() {
-            return data;
-        }
-
-        boolean hasLeft() {
-            return left != null;
-        }
-
-        boolean hasRight() {
-            return right != null;
-        }
-
-        void setData(T data) {
-            this.data = data;
-        }
-
-        TreeNode<T> getLeft() {
-            return left;
-        }
-
-        void setLeft(TreeNode<T> node) {
-            left = node;
-        }
-
-        TreeNode<T> getRight() {
-            return right;
-        }
-
-        void setRight(TreeNode<T> node) {
-            right = node;
         }
     }
 }
